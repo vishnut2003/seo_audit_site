a9d4d5e9fd68b81c6b1374e3f4cf1faab08370ca
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-01-23 15:30:23 +0530

    Add sign-out functionality to LogoutButton component

--- src/components/auth/LogoutButton.tsx
+++ src/components/auth/LogoutButton.tsx
@@ -1,16 +1,19 @@
 'use client';
 
 import { RiLogoutBoxRLine } from '@remixicon/react'
+import { signOut } from "next-auth/react"
 import React from 'react'
 
-const LogoutButton = ({iconType, className}: {
+const LogoutButton = ({ iconType, className }: {
     iconType: boolean,
     className: string
 }) => {
     return (
-        <button 
-        className={`${className} min-w-max`}>
-            { iconType && <RiLogoutBoxRLine size={24} /> }
+        <button
+            className={`${className} min-w-max`}
+            onClick={() => signOut()}
+        >
+            {iconType && <RiLogoutBoxRLine size={24} />}
             <p className='whitespace-nowrap'>Logout</p>
         </button>
     )
